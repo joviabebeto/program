ffaf03dee0b5a3b9b4c760a44f7928bd11ea81e8
joviabebeto <brigittetuyishimire5@gmail.com>
2023-07-24 09:00:55 +0200

    commerce

---
+++ script.js
@@ -0,0 +1,41 @@
+function menu(){
+   let nav = document.querySelector('.nav-links');
+if(nav.className === 'navlink'){
+    nav.classList +=" active"
+}else{
+    nav.className ='navlink'
+}
+}
+
+
+let country="bulundi";
+let continent="Africa";
+let population=20000;
+console.log(country);
+console.log(continent);
+console.log(population/2);
+
+population++;
+console.log(population);
+console.log(population>6);
+console.log(population<33);
+const description1=`${country} is in ${continent},and its ${population} million people speak ${language}`;
+country+
+'is in'+
+continent+
+',and its'+
+population+
+'million people speak'+
+language;
+console.log(description1);
+let isIsland=false;
+let language
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+language='English';
+
+console.log("9"-"4");
+console.log('30'-'20');
