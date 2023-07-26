62cdd5984668c883fe830aef73fb3dde9808bc8f
joviabebeto <brigittetuyishimire5@gmail.com>
2023-07-26 17:26:51 +0200

    add

--- spp.js
+++ spp.js
@@ -1,4 +1,20 @@
 
+const calcAge2=function(brithyeah){
+    return 2037-brithyeah;
+}
+const calcAge3=brithyeah => 2037-brithyeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+const yearsUnitlRetirenent= (brithyeah,firstName)=>{
+    const age = 2037-brithyeah;
+    const retirement=65-age;
+    // return retirement;
+    return `${firstname} retires in ${retirement}`
+    
+}
+
+
+
 
 function calcAge1(birthyear){
     return 2037-birthyear;
