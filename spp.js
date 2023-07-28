1d11b84a4e94d7886cb125633b344b0bda1a4f1c
joviabebeto <brigittetuyishimire5@gmail.com>
2023-07-28 15:11:11 +0200

    ok your good

--- spp.js
+++ spp.js
@@ -1,29 +1,41 @@
+// function cutfruitpieces(fruit){
+//     return fruit*4;
+// }
+// function fruitprocessor(apples,orange){
+//     const applepieces=cutfruitpieces(apples);
+//     const orangepieces=cutfruitpieces(orange);
 
-const calcAge2=function(brithyeah){
-    return 2037-brithyeah;
+//     const juice=`juice with ${applepieces} piece of apple and ${orangepieces} piece of orange.`;
+
+//     return juice;
+// }
+// console.log(fruitprocessor(2,3));
+
+// const calcAge=function(birthyeah){
+// return 2037-birthyeah;
+// }
+// const yearsUnitlRetirement=function(birthyeah,firstName){
+//     const age=calcAge(birthyeah);
+//     const retirement=65-age;
+//     if (retirement>=0){
+//         console.log(`${firstName} retires in ${retirement}years`);
+//         return retirement;
+       
+        
+//     }else{
+        
+//         console.log(`${firstName} has already retired 🎉`);
+//         return-1;
+        
+//     }
+// }
+// console.log(yearsUnitlRetirement(1991,'brigitte'));
+// console.log(yearsUnitlRetirement(1950,'brino'));
+const calcAverage=(score1,score2,score3)=>{
+    const Average=(score1+score2+score3/3);
+    return Average;
+
 }
-const calcAge3=brithyeah => 2037-brithyeah;
-const age3 = calcAge3(1991);
-console.log(age3);
-const yearsUnitlRetirenent= (brithyeah,firstName)=>{
-    const age = 2037-brithyeah;
-    const retirement=65-age;
-    // return retirement;
-    return `${firstname} retires in ${retirement}`
-    
-}
-
-
-
-
-function calcAge1(birthyear){
-    return 2037-birthyear;
-}
-const age1 = calcAge1(1991);
-console.log(age1);
-const calcAge2 = function(birthyear){
-    return 2037-birthyear;
-
-}
-const age2=calcAge2(1995);
-console.log(age1,age2);
+const scoreDolphins=calcAverage(65,98,53);
+const scorekoalas=calcAverage(34,68,82);
+const checkwinner=(AvgDolphins,)
