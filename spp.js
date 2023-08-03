7ff0f72d644866c3e8520c2de9a79796de7f6709
joviabebeto <brigittetuyishimire5@gmail.com>
2023-08-03 16:38:47 +0200

    hello

--- spp.js
+++ spp.js
@@ -1,41 +1,16 @@
-// function cutfruitpieces(fruit){
-//     return fruit*4;
-// }
-// function fruitprocessor(apples,orange){
-//     const applepieces=cutfruitpieces(apples);
-//     const orangepieces=cutfruitpieces(orange);
+const car={type:"Fiat", model:"500",color:"white"}
+console.log(car);
+const student={
+   firstName:'brino',
+    lastName:'bendo',
+    age:60,
+    eyecolor:'blue'
 
-//     const juice=`juice with ${applepieces} piece of apple and ${orangepieces} piece of orange.`;
-
-//     return juice;
-// }
-// console.log(fruitprocessor(2,3));
-
-// const calcAge=function(birthyeah){
-// return 2037-birthyeah;
-// }
-// const yearsUnitlRetirement=function(birthyeah,firstName){
-//     const age=calcAge(birthyeah);
-//     const retirement=65-age;
-//     if (retirement>=0){
-//         console.log(`${firstName} retires in ${retirement}years`);
-//         return retirement;
-       
-        
-//     }else{
-        
-//         console.log(`${firstName} has already retired 🎉`);
-//         return-1;
-        
-//     }
-// }
-// console.log(yearsUnitlRetirement(1991,'brigitte'));
-// console.log(yearsUnitlRetirement(1950,'brino'));
+};
+console.log(student);
 const calcAverage=(score1,score2,score3)=>{
-    const Average=(score1+score2+score3/3);
-    return Average;
-
+    const Average=(score1+score2+score3)/3;
+return Average;
 }
-const scoreDolphins=calcAverage(65,98,53);
-const scorekoalas=calcAverage(34,68,82);
-const checkwinner=(AvgDolphins,)
+const arrays=['nazif','emmy','rema']
+
